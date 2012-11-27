3a56b5a8aaaee9a6b92452b85a67bd88e8994faa
miah <miah@hoteltonight.com>
2012-11-26 16:51:41 -0800

    Use script rather than execute to ensure we have the right environment.

--- recipes/_graphite_syncdb.rb
+++ recipes/_graphite_syncdb.rb
@@ -3,17 +3,20 @@ template "#{node['graphite']['app_dir']}/initial_data.json" do
   owner node['graphite']['user']
   group node['graphite']['group']
   mode "0644"
-  variables (
-    {admin_user     => node['graphite']['db']['admin_user'],
-    admin_password => node['graphite']['db']['admin_password'],
-    admin_email    => node['graphite']['db']['admin_email']}
-  )
+  variables(
+    :admin_user     => node['graphite']['db']['admin_user'],
+    :admin_password => node['graphite']['db']['admin_password'],
+    :admin_email    => node['graphite']['db']['admin_email'])
 end
 
-execute "syncdb" do
-  command "#{node['graphite']['app_dir']}/manage.py syncdb --noinput"
+script "graphite_syncdb" do
+  code <<-EOF
+source #{ node['graphite']['virtualenv'] }/bin/activate
+python #{ node['graphite']['app_dir'] }/manage.py syncdb --noinput
+EOF
+  interpreter 'bash'
   user node['graphite']['user']
   cwd node['graphite']['user_home']
   action :run
-  not_if { File.exists?("#{node['graphite']['data_dir']}/graphite.db") }
+  not_if { File.exists?("#{ node['graphite']['data_dir'] }/django.sqlite3.db") }
 end
