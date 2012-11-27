6c003e6fe058a9f4eae04d8ec74a18a263798c51
miah <miah@hoteltonight.com>
2012-11-27 15:45:52 -0800

    pass caches variable which hhhas the contents of lookup_cache_resources. clean up spacing. file mode was executable for some reason.

--- recipes/_carbon_config.rb
+++ recipes/_carbon_config.rb
@@ -1,12 +1,13 @@
+caches = lookup_cache_resources
 
-template "#{node['graphite']['conf_dir']}/carbon.conf" do
+template "#{ node['graphite']['conf_dir'] }/carbon.conf" do
   source 'carbon.conf.erb'
   owner node['graphite']['user']
   group node['graphite']['group']
-  mode 0655
-  variables(
+  mode 0644
+  variables(:caches => caches,
+            :user => node['graphite']['user'],
             :line_rcvr_addr => node['graphite']['carbon']['line_rcvr_addr'],
             :pickle_rcvr_addr => node['graphite']['carbon']['pickle_rcvr_addr'],
-            :cache_query_addr => node['graphite']['carbon']['cache_query_addr']
-            )
+            :cache_query_addr => node['graphite']['carbon']['cache_query_addr'])
 end
