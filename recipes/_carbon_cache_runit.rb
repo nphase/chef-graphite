55b12e022cdd5bc9633878352ba42977ddcc2159
miah <miah@hoteltonight.com>
2012-11-28 16:28:34 -0800

    lookup carbon resources and set up services for them.

--- recipes/_carbon_cache_runit.rb
+++ recipes/_carbon_cache_runit.rb
@@ -1,8 +1,14 @@
 include_recipe 'runit'
 
-graphite_carbon_cache 'default' do
-  port 2003
-  pickle_port 2004
-  query_port 7002
-  listen_on '0.0.0.0'
+caches = lookup_resources(:graphite_carbon_cache)
+
+caches.each do |c|
+  runit_service "carbon-cache-#{ c.name }" do
+    template_name 'carbon-cache'
+    options :user => node['graphite']['user'],
+      :bin_dir => node['graphite']['bin_dir'],
+      :pid_dir => node['graphite']['pid_dir'],
+      :virt_env => node['graphite']['virtualenv'],
+      :name => c.name
+  end
 end
