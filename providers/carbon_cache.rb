647cfbee2b510e24383c9fd7e5220398026dac2e
miah <miah@hoteltonight.com>
2012-11-28 16:07:53 -0800

    remove runit_service from lwrp.

--- providers/carbon_cache.rb
+++ providers/carbon_cache.rb
@@ -1,18 +1,5 @@
 
 action :create do
-
-  cache_name = @new_resource.name
-
-  runit_service cache_name do
-    ignore_failure true
-    template_name 'carbon-cache'
-    options :user => node['graphite']['user'],
-      :bin_dir => node['graphite']['bin_dir'],
-      :pid_dir => node['graphite']['pid_dir'],
-      :virt_env => node['graphite']['virtualenv'],
-      :name => cache_name
-  end
-
 end
 
 action :delete do
